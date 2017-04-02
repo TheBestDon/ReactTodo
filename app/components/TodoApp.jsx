ad2f6083ef076a204adaeea4220ce66c72959181
TheBestDon <donatas_bakanas@yahoo.com>
2017-04-02 03:43:36 +0300

    Add toggle support

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -12,16 +12,20 @@ var TodoApp = React.createClass({
             todos: [
                 {
                     id: uuid(),
-                    text: 'walk the dog'
+                    text: 'walk the dog',
+                    completed: false
                 },  {
                     id: uuid(),
-                    text: 'feed the cat'
+                    text: 'feed the cat',
+                    completed: true
                 },   {
                     id: uuid(),
-                    text: 'shopping'
+                    text: 'shopping',
+                    completed: false
                 },   {
                     id: uuid(),
-                    text: 'hit the gym'
+                    text: 'hit the gym',
+                    completed: false
                 }
             ]
         };
@@ -32,7 +36,8 @@ var TodoApp = React.createClass({
                 ...this.state.todos,
                 {
                     id: uuid(),
-                    text: text
+                    text: text,
+                    completed: false
                 }
             ]
         });
@@ -43,13 +48,24 @@ var TodoApp = React.createClass({
             searchText: searchText.toLowerCase()
         })
     },
+    handleToggle: function (id) {
+        var updatedTodos = this.state.todos.map((todo) => {
+            if (todo.id === id) {
+                todo.completed = !todo.completed;
+            }
+
+            return todo;
+        });
+    this.setState({todos: updatedTodos});
+},
+
     render: function () {
         var {todos} = this.state;
 
         return (
             <div>
                 <TodoSearch onSearch={this.handleSearch}/>
-                <TodoList todos={todos}/>
+                <TodoList todos={todos} onToggle={this.handleToggle}/>
                 <AddTodo onAddTodo={this.handleAddTodo}/>
             </div>
         )
